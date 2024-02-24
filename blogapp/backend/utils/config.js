e50e0aed10a71f399b217c26904633af8143091b
FullStack-Tehtavien-Palautus <github-hy-fullstack@v.hix.fi>
2024-02-24 17:43:40 +0200

    vite. .js -> .jsx ... Uncaught TypeError: blog.user is undefined

--- blogapp/backend/utils/config.js
+++ blogapp/backend/utils/config.js
@@ -1,12 +1,17 @@
 require("dotenv").config();
 
-const PORT = process.env.PORT;
+/*const PORT = process.env.PORT;
 const MONGODB_URI =
   process.env.NODE_ENV === "test"
     ? process.env.TEST_MONGODB_URI
-    : process.env.MONGODB_URI;
+    : process.env.MONGODB_URI;*/
+    
+const PORT = 3003
+const MONGODB_URI = "mongodb+srv://githubhyfullstack:KHFoArk8Mx2f1PGA@cluster0.48clkfo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const SECRET='109365091623956108634871293845927634987562983746592643'
 
 module.exports = {
   MONGODB_URI,
   PORT,
+  SECRET,
 };
