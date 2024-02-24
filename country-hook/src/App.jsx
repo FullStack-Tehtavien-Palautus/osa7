07c925547391cb78da07b9aab16c1ebb5691b120
FullStack-Tehtavien-Palautus <github-hy-fullstack@v.hix.fi>
2024-02-24 12:02:07 +0200

    7.7: country hook

--- country-hook/src/App.jsx
+++ country-hook/src/App.jsx
@@ -18,30 +18,43 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {
+    if (name) axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
+      .then( resp => {
+        console.log(resp.data)
+        setCountry(resp)
+      })
+      .catch( () => {
+        setCountry(null)
+      })
+  }, [name])
 
   return country
 }
 
 const Country = ({ country }) => {
-  if (!country) {
-    return null
-  }
+  console.log(country)
 
-  if (!country.found) {
+  if (!country || country.status >= 400 ) {
     return (
       <div>
-        not found...
+        Not searched or not found...
       </div>
     )
   }
 
+  const name = country.data.name.common
+  const capital = country.data.capital[0]
+  const population = country.data.population
+  const flag = country.data.flag
+
   return (
     <div>
-      <h3>{country.data.name} </h3>
-      <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <h3>{name}</h3>
+      <div>capital {capital} </div>
+      <div>population {population}</div> 
+      <div style={{fontSize: '10em'}}>{flag}</div>
     </div>
   )
 }
